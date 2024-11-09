f42c4e6091228f6a67a7782da85535e36cea7f16
aaronmhowe <aaron.howe99@gmail.com>
2024-11-08 23:29:12 -0800

    implemented test cases for functions in EmailCode and DatabaseServer classes in totp_mfa.py

--- test/mfa_tests.py
+++ test/mfa_tests.py
@@ -4,6 +4,7 @@ from unittest.mock import Mock, patch
 import sqlite3
 from src.totp_mfa import TOTPMFA, EmailCode, DatabaseServer
 
+
 class TOTPMFATests(unittest.TestCase):
     """
     Test cases for the TOTPMFA class in totp_mfa.py
@@ -14,7 +15,7 @@ class TOTPMFATests(unittest.TestCase):
         Initializes a test email.
         """
         self.mfa = TOTPMFA()
-        self.test_email = "test@wsu.edu"
+        self.test_email = "user@wsu.edu"
 
     def code_length_is_six_ints(self):
         """
@@ -87,7 +88,7 @@ class TOTPMFATests(unittest.TestCase):
 
     def overwrite_stored_code(self):
         """
-        Tests when a new code is entered and store, the old code is overwritten
+        Tests when a new code is entered and store, the old code is overwritten.
         """
         self.mfa.store_mfa_code(self.test_email, "121212")
         code = self.mfa.database.get_code(self.test_email)
@@ -96,11 +97,82 @@ class TOTPMFATests(unittest.TestCase):
         new_code = self.mfa.database.get_code(self.test_email)
         self.assertEqual(new_code[0], "131313")
 
-class EmailCodeTests(unittest.TestCase):
-    pass
 
-class DatabaseServer(unittest.TestCase):
-    pass
+class EmailCodeTests(unittest.TestCase):
+    """
+    Test cases for the EmailCode class in totp_mfa.py
+    """
+    
+    def credentials(self):
+        """
+        Initializes a test admin email, test user email, and test user password.
+        """
+        self.email = EmailCode()
+        self.test_email = "user@wsu.edu"
+        self.email.send_from = "admin.wsu.edu"
+        self.email.password = "password"
+
+    @patch('smtplib.SMTP_SSL')
+    def sends_email(self, smtp):
+        """
+        Tests that an email is successfully sent to the user's email address.
+        """
+        send_email = self.email.send_to(self.test_email, "121212")
+        self.assertTrue(send_email)
+        smtp.return_value.send_message.assert_called_once()
+
+    def invalid_receiving_email(self):
+        """
+        Tests that an error is thrown when there is an attempt to send an email to invalid address.
+        """
+        with self.assertRaises(ValueError):
+            self.email.send_to("fake.email", "121212")
+        
+
+class DatabaseServerTests(unittest.TestCase):
+    """
+    Test cases for the DatabaseServer class in totp_mfa.py
+    """
+    
+    def server(self):
+        """
+        Initializes the database and a test user email.
+        """
+        self.database = DatabaseServer()
+        self.test_email = "user@wsu.edu"
+        self.database.create_tables()
+
+    def code_in_database(self):
+        """
+        Tests that verification codes are stored in the database.
+        """
+        self.database.store_code(self.test_email, "121212", datetime.now())
+        is_stored = self.database.get_code(self.test_email)
+        self.assertEqual(is_stored[0], "121212")
+    
+    def get_verification_code(self):
+        """
+        Tests fetching verification codes from the database.
+        """
+        self.database.store_code(self.test_email, "121212", datetime.now())
+        code, _ = self.database.get_code(self.test_email)
+        self.assertEqual(code, "121212")
+
+    def get_nonexistent_code_error(self):
+        """
+        Tests that attempting to retrieve a verification that doesn't exist throws an error.
+        """
+        code = self.database.get_code("test@wsu.edu")
+        self.assertIsNone(code)
+
+    def code_is_used(self):
+        """
+        Tests that a verification code is successfully marked as used once a user submits it.
+        """
+        self.database.store_code(self.test_email, "121212", datetime.now())
+        self.database.is_used(self.test_email, "121212")
+        code = self.database.get_code(self.test_email)
+        self.assertIsNone(code)
 
 if __name__ == '__main__':
     unittest.main()
